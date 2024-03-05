da434e8b27b0588edbc9a08d9a7c42ead352beae
Louchin <cejarlouchin@gmail.com>
2024-03-05 15:23:24 +0800

    added TestNG parallel test

--- src/test/java/cucumberOptions/TestNGRunner.java
+++ src/test/java/cucumberOptions/TestNGRunner.java
@@ -1,10 +1,12 @@
 package cucumberOptions;
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 import org.junit.runner.RunWith;
+import org.testng.annotations.DataProvider;
 
-@RunWith(Cucumber.class)
+/*@RunWith(Cucumber.class)*/
 @CucumberOptions(
         // Path to your feature files
         features = "src/test/java/features",
@@ -14,8 +16,12 @@ import org.junit.runner.RunWith;
         // Specify the report formats
         plugin = {"pretty", "html:target/cucumber-reports.html"}
         //for specified scenarios only
-        ,tags = "@Test5"
+        //,tags = "@Test5"
 )
-public class TestNGRunner {
-
+public class TestNGRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
